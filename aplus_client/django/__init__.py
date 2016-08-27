d631dd8eb5200636046fc2294699f2752d5e3c45
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2016-08-27 05:21:11 +0300

    (mooc-jutut@ac1d489) Create aplus_client.django a real django app

--- aplus_client/django/__init__.py
+++ aplus_client/django/__init__.py
@@ -2,3 +2,5 @@ try:
     import django
 except ImportError:
     raise ImportError("aplus_client.django requires django and is only usful with it")
+
+default_app_config = 'aplus_client.django.apps.AplusClientConfig'
