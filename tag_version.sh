7b7b8b8ddc8239ee4a860aba1649cb18b90919f7
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2019-04-18 15:03:45 +0300

    Update tag_version.sh

--- tag_version.sh
+++ tag_version.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+cd "$(dirname "$0")"
 
-version=$(grep version setup.py|cut -d"'" -f 2)
-git tag -a $version -m "v$version"
-git push --tags
+version=$(grep -E '^__version__\s*=' aplus_client/__init__.py|cut -d"'" -f 2)
+if [ "x${version#*-}" = "x$version" ]; then
+	git tag -a v$version -m "Release $version"
+else
+	git tag -a v$version -m "Pre-release $version"
+fi
+echo "Remember to push tags: git push --tags"
